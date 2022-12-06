aa7723ac8f1496369af86702a6a8559c1c52ceed
Adam Hrbac <xhrbac12@vutbr.cz>
2022-12-06 10:28:22 +0100

    Day 6

---
+++ 6/s.pl
@@ -0,0 +1,20 @@
+:- use_module(library(lists)).
+:- use_module(library(clpz)).
+:- use_module(library(iso_ext)).
+:- use_module(library(pio)).
+:- use_module(library(dcgs)).
+
+inp(Xs) --> seq(X), eol, {maplist(char_code, X, Xs)}.
+eol --> "\n".
+is_solution(As) :-
+	all_distinct(As).
+solve(A, N, Xs) :-
+	length(W, N),
+	length(L, A),
+	append(_, W, L),
+	append(L, _, Xs),
+	is_solution(W).
+solve(A, B) :-
+	phrase_from_file(inp(Xs), 'input'),
+	solve(A, 4, Xs),
+	solve(B, 14, Xs).
